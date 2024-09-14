cf08ebd971b1e8fc802711736c3192b58d2b0d39
Adi Yakoby <adi.yakoby@gmail.com>
2024-09-14 11:48:26 +0300

    fixed bugs

--- GdJobScarpper.py
+++ GdJobScarpper.py
@@ -19,7 +19,7 @@ class JobScraper:
     def __init__(self, url):
         # Set up Chrome options
         self.chrome_options = Options()
-        # chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+        # self.chrome_options.add_argument("--headless")  # Run Chrome in headless mode
         self.chrome_options.add_argument("--no-sandbox")
         self.chrome_options.add_argument("--disable-dev-shm-usage")
         self.chrome_options.add_argument("--window-size=1920,1080")
@@ -38,9 +38,9 @@ class JobScraper:
             close_button = WebDriverWait(self.driver, random.randrange(1, 3)).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, 'button.CloseButton'))
             )
-            if close_button.is_displayed():
+            if close_button and close_button.is_displayed():
                 close_button.click()
-            print("Closed the pop-up message.")
+                # print("Closed the pop-up message.")
         except Exception:
             pass
 
@@ -49,12 +49,13 @@ class JobScraper:
             load_more_button = WebDriverWait(self.driver, random.randrange(1, 3)).until(
                 EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-test="load-more"]'))
             )
-            load_more_button.click()
-            # print("Clicked the 'Show more jobs' button.")
-            time.sleep(random.random()+1)
+            if load_more_button and load_more_button.is_displayed():
+                load_more_button.click()
+                # print("Clicked the 'Show more jobs' button.")
+            time.sleep(random.random())
             return True
         except Exception as e:
-            print(f"No 'Show more jobs' button or error clicking it: {e}")
+            # print(f"No 'Show more jobs' button or error clicking it: {e}")
             return False
 
     def click_show_more_description(self):
@@ -62,9 +63,10 @@ class JobScraper:
             show_more_button = WebDriverWait(self.driver, random.randrange(1, 3)).until(
                 EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class^='JobDetails_showMore']"))
             ) 
-            show_more_button.click()
-            print("Clicked the 'Show more' button in the job description.")
-            time.sleep(random.random()+1) # Wait for the description to expand
+            if show_more_button and show_more_button.is_displayed():
+                show_more_button.click()
+            # print("Clicked the 'Show more' button in the job description.")
+            time.sleep(random.random()) # Wait for the description to expand
         except Exception:
             pass
 
@@ -72,61 +74,62 @@ class JobScraper:
         jobs_data = []
 
         for _ in range(pages):
-            try:
-                # Wait for job cards to be present
-                job_elements = WebDriverWait(self.driver, 20).until(
-                    EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'li[data-test="jobListing"]'))
-                )
-                
-                print(f"Found {len(job_elements)} job listings.")
-                
-                for job in job_elements:
-                    try:
-                        job_title = job.find_element(By.CSS_SELECTOR, 'a[data-test="job-title"]').text
-                        company_name = job.find_element(By.CSS_SELECTOR, 'div[class^="EmployerProfile_employerNameContainer"]').text
-                        location = job.find_element(By.CSS_SELECTOR, '[data-test="emp-location"]').text
-                        job_link = job.find_element(By.CSS_SELECTOR, 'a[data-test="job-title"]').get_attribute('href')
-                        job_age = job.find_element(By.CSS_SELECTOR, 'div[class^="JobCard_listingAge"]').text
+            self.handle_pop_up()
+            self.click_load_more()
 
-                        job.click()
-                        self.handle_pop_up()
-                        self.click_show_more_description()
-
-                        description_div = WebDriverWait(self.driver, 10).until(
-                            EC.presence_of_element_located((By.CSS_SELECTOR, 'div[class^="JobDetails_jobDescription"]'))
-                        )
-                        description_text = description_div.text
-
-                        job_data = {
-                            "Job Title": job_title,
-                            "Company Name": company_name,
-                            "Location": location,
-                            "Job Link": job_link,
-                            "Job Age": job_age,
-                            "Job Description": description_text
-                        }
-
-                        jobs_data.append(job_data)
-
-                         # Print job details
-                        # print(f"Job Title: {job_title}")
-                        # print(f"Company Name: {company_name}")
-                        # print(f"Location: {location}")
-                        # print(f"Job Link: {job_link}")
-                        # print(f"Job Description:\n{description_text}")
-                        # print("-" * 60)
+        try:
+            # Wait for job cards to be present
+            job_elements = WebDriverWait(self.driver, 20).until(
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'li[data-test="jobListing"]'))
+            )
             
-                    except Exception as e:
-                        print(f"Error extracting job data: {e}")
+            print(f"Found {len(job_elements)} job listings.")
+            
+            for job in job_elements:
+                try:
+                    job_title = job.find_element(By.CSS_SELECTOR, 'a[data-test="job-title"]').text
+                    company_name = job.find_element(By.CSS_SELECTOR, 'div[class^="EmployerProfile_employerNameContainer"]').text
+                    location = job.find_element(By.CSS_SELECTOR, '[data-test="emp-location"]').text
+                    job_link = job.find_element(By.CSS_SELECTOR, 'a[data-test="job-title"]').get_attribute('href')
+                    job_age = job.find_element(By.CSS_SELECTOR, 'div[class^="JobCard_listingAge"]').text
+
+                    job.click()
+                    self.handle_pop_up()
+                    self.click_show_more_description()
+
+                    description_div = WebDriverWait(self.driver, 10).until(
+                        EC.presence_of_element_located((By.CSS_SELECTOR, 'div[class^="JobDetails_jobDescription"]'))
+                    )
+                    description_text = description_div.text
+
+                    job_data = {
+                        "Job Title": job_title,
+                        "Company Name": company_name,
+                        "Location": location,
+                        "Job Link": job_link,
+                        "Job Age": job_age,
+                        "Job Description": description_text
+                    }
+
+                    jobs_data.append(job_data)
+
+                        # Print job details
+                    # print(f"Job Title: {job_title}")
+                    # print(f"Company Name: {company_name}")
+                    # print(f"Location: {location}")
+                    # print(f"Job Link: {job_link}")
+                    # print(f"Job Description:\n{description_text}")
+                    # print("-" * 60)
+        
+                except Exception as e:
+                    print(f"Error extracting job data: {e}")
 
 
-                # Try to click the "Show more jobs" button, break the loop if not available
-                if not self.click_load_more():
-                    break
+        
 
-            finally:
-                # Quit the browser after scraping
-                self.driver.quit()
-                return jobs_data
+        finally:
+            # Quit the browser after scraping
+            self.driver.quit()
+            return jobs_data
         return jobs_data
 
--- app.py
+++ app.py
@@ -5,16 +5,11 @@ from JobDataCollector import JobDataCollector
 if __name__ == "__main__":
     job_titles = ['software engineer', 'fullstack', 'fullstack developer', 'junior', 'junior developer']
 
-    # for job_title in job_titles:
-    #     scraper = JobScraper(generate_url(job_title))
+    for job_title in job_titles:
+        scraper = JobScraper(generate_url(job_title))
     
-    #     scraper.start_browser()
-    #     jobs = scraper.extract_job_details()
-    #     data_colletor = JobDataCollector(job_title,jobs)
-    #     data_colletor.save_to_csv()
-    scraper = JobScraper(generate_url(job_titles[1]))
-
-    scraper.start_browser()
-    jobs = scraper.extract_job_details()
-    data_colletor = JobDataCollector(job_titles[1],jobs)
-    data_colletor.save_to_csv()
+        scraper.start_browser()
+        jobs = scraper.extract_job_details(2)
+        data_colletor = JobDataCollector(job_title,jobs)
+        data_colletor.save_to_csv()
+ 
